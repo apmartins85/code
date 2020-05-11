efedd9b105a20f01fd2a2a27c17d47fc688a238e
Pasqualinoto, Andre Martins <andre.martins@wirecard.com>
2020-05-11 15:40:34 -0300

    added complete project

---
+++ parte2/terraformEnv/user-data/dockerImage.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+yum -y install epel-release git && \
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo && \
+yum -y install docker-ce && \
+yum -y clean all
+
+mkdir /root/.docker
+
+cat > /root/.docker/config.json << EOL
+{
+	"auths": {
+		"https://index.docker.io/v1/": {
+			"auth": "lalalala"
+		}
+	}
+}
+EOL
+
+docker pull apmartins85/helloworld-kotlin
+
+docker run --name helloworld-kotlin -d -t --rm --privileged -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
+	-p 8080:8080 apmartins85/helloworld-kotlin:1.0.0
